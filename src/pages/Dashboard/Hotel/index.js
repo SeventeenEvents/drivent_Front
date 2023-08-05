c89f6981cf116c0e37d6ccc8e0306feaba5c596c
João Paulo Vargas <joao.vargas@seatecnologia.com.br>
2023-08-05 10:27:45 -0300

    verificação de pagamento da inscrição para exibir as opções de hospedagem

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,42 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import UserContext from '../../../contexts/UserContext';
+import { useContext } from 'react';
+
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+  const { userData } = useContext(UserContext);
+  const { token } = userData;
+  const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+  const [ticketData, setTicketData] = useState([]);
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }; 
+
+  console.log(ticketData);
+  
+  useEffect(() => {
+    axios.get(`${BASE_URL}tickets`, config).then((response) => {
+      setTicketData(response.data);
+    }).catch((error) => {
+      console.log(error.message);
+    });
+  }, []);
+
+  function verifyUserTicketStatus() {
+    if(ticketData.status === 'PAID') {
+      return 'Listando hoteis ..';
+    } else if (ticketData.status === 'RESERVED')  {
+      return 'Pagamento pendente. Finalize o pagamento para poder acessar os hotéis disponíveis.';
+    } else if (ticketData.length === 0) {
+      return 'Ainda não há inscrição em nenhum evento.';
+    }
+  }
+
+  return (
+    <>
+      {verifyUserTicketStatus()}
+    </>
+  );
 }
