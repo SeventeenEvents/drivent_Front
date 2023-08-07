638501e1041868ff425bc3a7abcc8f1cbe0f27e7
guilfornari <120119237+guilfornari@users.noreply.github.com>
2023-08-07 20:51:51 -0300

    Revert "Feat/modalities with without hotel"

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,15 +1,5 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import useToken from '../../../hooks/useToken';
-import TicketBox from '../../../components/Tickets/TicketBox';
-import HotelBox from '../../../components/Tickets/HotelBox';
-import { getPersonalInformations } from '../../../services/enrollmentApi';
-import { toast } from 'react-toastify';
-import useTicketTypes from '../../../hooks/api/useTicketTypes';
-import api from '../../../services/api'; // Importe o serviço de API, se não estiver importado.
-
-import { useEffect, useState } from 'react';
-import styled from 'styled-components';
 import api from '../../../services/api';
 import useToken from '../../../hooks/useToken';
 import TicketBox from '../../../components/Tickets/TicketBox';
@@ -20,120 +10,47 @@ export default function Payment() {
   const [selected, setSelected] = useState([]);
   const [show, setShow] = useState(false);
   const [price, setPrice] = useState(0);
-  const [includesHotel, setIncludesHotel] = useState(false);
-  const [isRemote, setIsRemote] = useState(false);
-  const [selectedHotel, setSelectedHotel] = useState([]);
-  const [hasEnrollment, setHasEnrollment] = useState(false);
 
-  // New
-  const [ticketTypeId, setTicketTypeId] = useState(0);
-  const { ticketTypes } = useTicketTypes();
-
-  useEffect(() => {
-    setTypes(ticketTypes);
-  }, [ticketTypes]);
-
-  useEffect(() => {
-    async function checkUserEnrollment() {
-      try {
-        const personalInformations = await getPersonalInformations(token);
-        setHasEnrollment(personalInformations !== null);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
-    checkUserEnrollment();
-  }, [token]);
-
-  async function submit() {
-    const enrrolmentId = await api.get('/enrollments', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    const body = {
-      ticketTypeId: ticketTypeId,
-      enrrolmentId: enrrolmentId.data.id,
-      status: 'RESERVED'
-    };
-
-    console.log(body);
-
+  async function getTypes() {
     try {
-      const response = await api.post('/tickets', body, {
+      const response = await api.get('/tickets/types', {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
-
-      toast.success('Ingresso comprado com sucesso!');
+      setTypes(response.data);
     } catch (err) {
-      toast.error('Erro ao comprar ingresso');
+
     }
   }
 
+  useEffect(() => {
+    getTypes();
+  }, []);
+
   return (
-    <>
-      {hasEnrollment ? (
-        <>
-          <TicketsPageContainer>
-            <div>
-              <h1>Ingresso e Pagamento</h1>
-              <h2>Primeiro escolha sua modalidade de pagamento</h2>
-            </div>
-            <TicketsContainer>
-              {types.map((type) => (
-                <TicketBox
-                  key={type.id}
-                  name={type.name}
-                  price={type.price}
-                  id={type.id}
-                  isRemote={type.isRemote}
-                  setShow={setShow}
-                  setPrice={setPrice}
-                  selected={selected}
-                  setSelected={setSelected}
-                  setIncludesHotel={setIncludesHotel}
-                  setIsRemote={setIsRemote}
-                  selectedHotel={selectedHotel}
-                  setTicketTypeId={setTicketTypeId}
-                  type={type}
-                />
-              ))}
-            </TicketsContainer>
-            {!isRemote && includesHotel && (
-              <>
-                <div>
-                  <h2>Ótimo! Agora escolha sua modalidade de hospedagem</h2>
-                </div>
-                <HotelsContainer>
-                  <HotelBox
-                    includesHotel={true}
-                    setShow={setShow}
-                    setPrice={setPrice}
-                    selectedHotel={selectedHotel}
-                    setSelectedHotel={setSelectedHotel}
-                    setTicketTypeId={setTicketTypeId}
-                  />
-                </HotelsContainer>
-              </>
-            )}
-            <CheckOutContainer show={show}>
-              <h3>
-                Fechado! O total ficou em <span>R$ {price}</span>. Agora é só confirmar:
-              </h3>
-              <button onClick={submit}>RESERVAR INGRESSO</button>
-            </CheckOutContainer>
-          </TicketsPageContainer>
-        </>
-      ) : (
-        <ErrorContainer>
-          <h3> Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso </h3>
-        </ErrorContainer>
-      )}
-    </>
+    <TicketsPageContainer>
+      <div>
+        <h1>Ingresso e Pagamento</h1>
+        <h2>Primeiro escolha sua modalidade de pagamento</h2>
+      </div>
+      <TicketsContainer>
+        {types.map((type) => (<TicketBox
+          key={type.id}
+          name={type.name}
+          price={type.price}
+          id={type.id}
+          isRemote={type.isRemote}
+          setShow={setShow}
+          setPrice={setPrice}
+          selected={selected}
+          setSelected={setSelected} />))}
+      </TicketsContainer>
+      <CheckOutContainer show={show}>
+        <h3>Fechado! O total ficou em <span>R$ {price}</span>. Agora é só confirmar:</h3>
+        <button>RESERVAR INGRESSO</button>
+      </CheckOutContainer>
+    </TicketsPageContainer>
   );
 }
 
@@ -150,16 +67,11 @@ const TicketsPageContainer = styled.div`
     margin-top: 34px;
     font-size: 20px;
     font-weight: 400;
-    color: #8e8e8e;
+    color: #8E8E8E
   }
 `;
 
 const TicketsContainer = styled.div`
-  margin-top: 15px;
-  display: flex;
-`;
-
-const HotelsContainer = styled.div`
   margin-top: 15px;
   display: flex;
 `;
@@ -170,7 +82,7 @@ const CheckOutContainer = styled.div`
   flex-direction: column;
   h3 {
     margin-top: 30px;
-    color: #8e8e8e;
+    color: #8E8E8E;
     font-size: 20px;
     font-weight: 400px;
   }
@@ -187,29 +99,11 @@ const CheckOutContainer = styled.div`
     border-radius: 4px;
     font-size: 13px;
     background-color: #e0e0e0;
-    box-shadow: 0px 2px 10px 2px rgba(221, 225, 230, 0.94);
+    box-shadow: 0px 2px 10px 2px rgba(221,225,230,0.94);
     cursor: pointer;
 
     :hover {
-      border: solid 2px #cecece;
+      border: solid 2px #CECECE;
     }
   }
-`;
-
-const ErrorContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  width: 100%;
-  height: 80%;
-
-  div {
-    width: 50%;
-  }
-
-  font-size: 20px;
-  font-weight: 400;
-  color: #8e8e8e;
-  line-height: 24px;
 `;
