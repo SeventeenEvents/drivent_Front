ce881b65f3ad3326115aad3bec43e9c4200003b4
João Paulo Vargas <joao.vargas@seatecnologia.com.br>
2023-08-13 10:40:43 -0300

    correção renderização quando o usuário faz a reserva

--- src/components/Hotel/HotelsList.js
+++ src/components/Hotel/HotelsList.js
@@ -5,18 +5,18 @@ import BookingResume from './BookingResume';
 import RoomList from './RoomList';
 import useToken from '../../hooks/useToken';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 export default function HotelsList({ hotels }) {
   const [selectedHotel, setSelectedHotel] = useState(null);
   const token = useToken();
   const [selectedRoom, setSelectedRoom] = useState();
   const [userBooking, setUserBooking] = useState([]);
-  console.log('Hotel selecionado: ' + selectedHotel + 'Quarto selecionado' + selectedRoom);
+  const [bookingChange, setBookingChange] = useState(false);
 
   function toggleHotel(hotelId) {
     if (hotelId === selectedHotel) setSelectedHotel();
     if (hotelId !== selectedHotel) setSelectedHotel(hotelId);
-    console.log('Hotel selecionado: ' + selectedHotel + 'Quarto selecionado' + selectedRoom);
   }
   const headers = {
     headers: {
@@ -33,7 +33,7 @@ export default function HotelsList({ hotels }) {
       .catch((error) => {
         console.log(error.message);
       });
-  }, []);
+  }, [bookingChange]);
 
   function sendBooking() {
     const body = {
@@ -44,7 +44,8 @@ export default function HotelsList({ hotels }) {
     const promise = axios
       .post(`${process.env.REACT_APP_API_BASE_URL}/booking`, body, headers)
       .then(() => {
-        alert('Reserva de quarto efetuada com sucesso!');
+        toast.success('Reserva de quarto efetuada com sucesso!');
+        setBookingChange(true);
       })
       .catch((error) => {
         console.log(error.message);
@@ -66,17 +67,16 @@ export default function HotelsList({ hotels }) {
               />
             ))}
           </ListContainer>
+          {selectedHotel && (
+            <RoomList selectedRoom={selectedRoom} setSelectedRoom={setSelectedRoom} selectedHotel={selectedHotel} />
+          )}
+
+          {selectedRoom != null && selectedHotel != null && (
+            <SendBookingButton onClick={sendBooking}>RESERVAR QUARTO</SendBookingButton>
+          )}
         </div>
       ) : (
         <BookingResume userBooking={userBooking} setUserBooking={setUserBooking} />
-      )}
-
-      {selectedHotel && (
-        <RoomList selectedRoom={selectedRoom} setSelectedRoom={setSelectedRoom} selectedHotel={selectedHotel} />
-      )}
-
-      {selectedRoom != null && selectedHotel != null && (
-        <SendBookingButton onClick={sendBooking}>RESERVAR QUARTO</SendBookingButton>
       )}
     </>
   );
